fd4fef47f7fb138245564fbb674a311f12f2b316
Rei <sovietspaceship@users.noreply.github.com>
2017-07-10 20:57:55 +0100

    fixed docstring, thanks cage2
    https://notabug.org/cage/cl-telegram-bot/commit/43b7d41702cf2cf1d58fd67176ff1d649eefb2fb

--- cl-telegram-bot.lisp
+++ cl-telegram-bot.lisp
@@ -320,7 +320,10 @@
         (make-request b "getFile" options)))
 
 (defun download-file (b file-id)
-  "Perform HTTP request to 'name API method with 'options JSON-encoded object."
+  "Get the  path for a  file from a  file-id (see: get-file)  and then
+   download it.  Returns nil if the value of the http response code is
+   not  success (200);  otherwise it  will returns  three values:  the
+   data, the http headers and the exension of the original file"
   (with-package :cl-telegram-bot
     (let* ((file-spec (decode (get-file b file-id))))
       (with-ok-results (file-spec results)
