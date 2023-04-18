9dbd27886b8341bd9804d119d7a0a1b51f148777
Alexander Artemenko <svetlyak.40wt@gmail.com>
2023-04-18 13:19:59 +0400

    Moved docs from README.md and ChangeLog.rst

--- ChangeLog.md
+++ ChangeLog.md
@@ -2,11 +2,55 @@
 
 # ChangeLog
 
+<a id="x-28CL-TELEGRAM-BOT-DOCS-2FCHANGELOG-3A-3A-7C0-2E3-2E2-7C-2040ANTS-DOC-2FLOCATIVES-3ASECTION-29"></a>
+
+## 0.3.2 (2022-07-10)
+
+* Change the parameters of `make-request` to allow passing request parameters straight into it.
+
+* Add `id` slot to `message`; add `forward-message`, `delete-message` functions reliant on `id`.
+
+* Add more `message` types: `reply`, `video-message`, `document-message` and other media message types.
+
+* Add `send-*` media-sending message types.
+
+* Add more `chat` types: `group`, `supergroup`, and `channel`.
+
+<a id="x-28CL-TELEGRAM-BOT-DOCS-2FCHANGELOG-3A-3A-7C0-2E3-2E1-7C-2040ANTS-DOC-2FLOCATIVES-3ASECTION-29"></a>
+
+## 0.3.1 (2020-01-04)
+
+* Fixed work with latest `dexador`, because it does not accept `:stream t` anymore.
+
+<a id="x-28CL-TELEGRAM-BOT-DOCS-2FCHANGELOG-3A-3A-7C0-2E3-2E0-7C-2040ANTS-DOC-2FLOCATIVES-3ASECTION-29"></a>
+
+## 0.3.0 (2019-09-22)
+
+* Bot was fixed to use latest Dexador with support
+  of `read-timeout` and `connect-timeout`.
+
+<a id="x-28CL-TELEGRAM-BOT-DOCS-2FCHANGELOG-3A-3A-7C0-2E2-2E0-7C-2040ANTS-DOC-2FLOCATIVES-3ASECTION-29"></a>
+
+## 0.2.0 (2019-09-16)
+
+* Added a dependency from `trivial-timeout` and now connect timeout is used when
+  doing requests to `API`.
+
+* Function `make-<bot-class>` now proxie any parameters to the class's constructor.
+
+* Now function `stop-processing` checks if thread is alive before destroying it.
+
 <a id="x-28CL-TELEGRAM-BOT-DOCS-2FCHANGELOG-3A-3A-7C0-2E1-2E0-7C-2040ANTS-DOC-2FLOCATIVES-3ASECTION-29"></a>
 
-## 0.1.0 (2023-02-05)
+## 0.1.0 (2018-09-17)
 
-* Initial version.
+<a id="refactorings"></a>
+
+### Refactorings
+
+Project was broken down to subpackages, nicknames `telegram-bot` and
+`tg-bot` were removed, because now system uses `ASDF`'s
+package-inferred-system class and each file have it's own separate packages.
 
 
 * * *
--- docs/index.lisp
+++ docs/index.lisp
@@ -32,8 +32,7 @@
   
   (list :theme
         (find-symbol "40ANTS-THEME"
-                     (find-package "40ANTS-DOC-THEME-40ANTS")))
-  )
+                     (find-package "40ANTS-DOC-THEME-40ANTS"))))
 
 
 (defsection @index (:title "cl-telegram-bot - Telegram Bot API, based on sovietspaceship's work but mostly rewritten."
@@ -42,6 +41,7 @@
                                    "TODO"
                                    "MIT"
                                    "API"
+                                   "CLOS"
                                    "REPL"
                                    "GIT"))
   (cl-telegram-bot system)
@@ -51,7 +51,8 @@
 ![Quicklisp](http://quickdocs.org/badge/cl-telegram-bot.svg)
 "
   (@installation section)
-  (@usage section))
+  (@quickstart section)
+  (@credits section))
 
 
 (defsection-copy @readme @index)
@@ -69,10 +70,75 @@ You can install this library from Quicklisp, but you want to receive updates qui
 """)
 
 
-(defsection @usage (:title "Usage"
-                    :ignore-words ("ASDF:PACKAGE-INFERRED-SYSTEM"
-                                   "ASDF"
-                                   "40A"))
+(defsection @quickstart (:title "Quickstart")
   "
-TODO: Write a library description. Put some examples here.
+The system uses CLOS to add new methods to process incoming messages.
+To create a simple bot, all you need is to define `on-message` method.
+
+If you want to match on a particular command, like `/help` or `/make-me-happy 7 times`,
+then you better to define a `on-command` method.
+
+During messages processing, function `(reply \"some text\")` is available, which will send
+given text into the right chat. Also, there is `send-message` and other function exists
+which allow your bot to post messages, images and other media into the any chat.
+
+Here is example of a simple bot which reacts on the text message and `/echo` command:
+
+```lisp
+CL-USER> (defpackage the-bot (:use :cl :cl-telegram-bot))
+#<Package \"THE-BOT\">
+CL-USER> (in-package the-bot)
+#<Package \"THE-BOT\">
+THE-BOT> (defbot echo-bot)
+MAKE-ECHO-BOT
+THE-BOT> (defmethod on-message ((bot echo-bot)
+                                text)
+           (reply text))
+#<STANDARD-METHOD ON-MESSAGE (ECHO-BOT T)>
+THE-BOT> (defmethod on-command ((bot echo-bot)
+                                (command (eql :help))
+                                text)
+           (declare (ignorable text))
+           (reply \"Just send me any text and I'll reply with the same text.\"))
+#<STANDARD-METHOD ON-COMMAND (ECHO-BOT (EQL :HELP) T)>
+THE-BOT> (defmethod on-command ((bot echo-bot)
+                                (command (eql :start))
+                                text)
+           (declare (ignorable text))
+           (reply \"Welcome Lisper! Have a fun, playing with cl-telegram-bot!\"))
+#<STANDARD-METHOD ON-COMMAND (ECHO-BOT (EQL :START) T)>
+```
+
+Now, stop for the minute, open your Telegram client, and create a new bot
+using the BotFather bot:
+
+![](images/create-a-bot.png)
+
+When you've got token, return to the REPL and start our bot:
+
+```
+THE-BOT> (start-processing (make-echo-bot \"5205125**********************************\")
+                           :debug t)
+ <INFO> [08:31:09] cl-telegram-bot core.lisp (start-processing) - Starting thread to process updates for CL-TELEGRAM-BOT/CORE::BOT: #<ECHO-BOT id=0> 
+#<PROCESS telegram-bot(33) [Reset] #x30200709246D>
+THE-BOT> 
+```
+
+This will start a new thread for processing incoming messages.
+
+Now, find your bot in the Telegram client:
+
+![](images/choose-the-bot.png)
+
+And start communicating with him:
+
+![](images/write-to-the-bot.png)
+
+")
+
+
+(defsection @credits (:title "Credits")
+  "
+* [Rei](https://github.com/sovietspaceship) – initial version.
+* [Alexander Artemenko](https://github.com/svetlyak40wt) – large refactoring, usage of CLOS classes, etc.
 ")
