d31f620f9a3132da3104a517dc344019d2c71185
rtmpdavid <rtmpdavid@gmail.com>
2024-02-16 16:46:03 +0300

    Add a way of sending multi-part data with pathnames

--- src/network.lisp
+++ src/network.lisp
@@ -23,9 +23,21 @@
   (:report (lambda (condition stream)
              (format stream "Request error: ~A" (what condition)))))
 
+(defun process-options (&rest options &key (pathname-p nil) &allow-other-keys)
+  (loop for (key value)
+          on (alexandria:remove-from-plist options :timeout :streamp :pathname-p)
+	by #'cddr
+	when value
+	  if pathname-p
+	    collect (cons (kebab:to-snake-case key) value)
+	else
+	  collect (kebab:to-snake-case key)
+	  and
+            collect value))
 
-(defun make-request (bot name &rest options &key (streamp nil) (timeout 3) &allow-other-keys)
-  "Perform HTTP request to 'name API method with 'options JSON-encoded object."
+
+(defun make-request (bot name &rest options &key (streamp nil) (timeout 3) (pathname-p nil) &allow-other-keys)
+  "Perform HTTP request to 'name API method with 'options JSON-encoded object or multi-part form-data."
   (declare (ignore streamp))
 
   (let ((url (concatenate 'string (get-endpoint bot) name)))
@@ -33,26 +45,14 @@
                (obfuscate url)
                options)
     (let* ((max-timeout (* timeout 10))
-           (processed-options (loop for (key value)
-                                      on (alexandria:remove-from-plist options :timeout :streamp)
-                                        by #'cddr
-                                    when value
-                                      collect (kebab:to-snake-case key)
-                                      and
-                                        collect value))
+           (processed-options (apply #'process-options options))
            (response
-             (if *proxy*
-                 (dexador:post url
-                               :headers '(("Content-Type" . "application/json"))
-                               :content (jonathan:to-json processed-options)
-                               :read-timeout max-timeout
-                               :connect-timeout max-timeout
-                               :proxy *proxy*)
-                 (dexador:post url
-                               :headers '(("Content-Type" . "application/json"))
-                               :content (jonathan:to-json processed-options)
-                               :read-timeout max-timeout
-                               :connect-timeout max-timeout)))
+             (dexador:post url
+                           :headers (unless pathname-p '(("Content-Type" . "application/json")))
+                           :content (if pathname-p processed-options (jonathan:to-json processed-options))
+                           :read-timeout max-timeout
+                           :connect-timeout max-timeout
+                           :proxy (if *proxy* *proxy* dex:*default-proxy*)))
            (data (jonathan:parse response)))
       (unless (getf data :|ok|)
         (log:error "Wrong data received from the server" data)
